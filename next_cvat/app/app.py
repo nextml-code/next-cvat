d870532bbf1b7b84bdbe357d4e47a6dcaa9f1090
Richard Löwenström <samedii@gmail.com>
2024-11-05 18:04:55 +0100

    fix: app commands working

--- next_cvat/app/app.py
+++ next_cvat/app/app.py
@@ -1,13 +1,12 @@
 import typer
 
-from .create_token import create_token as create_token_command
-from .download import download as download_command
+from .create_token import create_token
+from .download import download
 
 app = typer.Typer(
     name="next-cvat",
     help="CLI tool for downloading and handling CVAT annotations",
 )
 
-
-app.command(create_token_command)
-app.command(download_command)
+app.command()(create_token)
+app.command()(download)
