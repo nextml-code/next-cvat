1063347eeb41ab25c7ef78631d6f86547599c8c9
Richard Löwenström <samedii@gmail.com>
2024-11-05 16:35:31 +0100

    feature: client working and refactor access token

--- next_cvat/create_token.py
+++ next_cvat/create_token.py
@@ -1,35 +1,33 @@
-import base64
-import json
-
 from cvat_sdk import make_client
+from pydantic_settings import BaseSettings
+
+from .access_token import AccessToken
 
 
 def create_token(username: str, password: str) -> str:
     with make_client(host="app.cvat.ai") as client:
         client.login((username, password))
 
-        token_data = dict(
-            sessionid=client.api_client.cookies["sessionid"],
-            csrftoken=client.api_client.cookies["csrftoken"],
-            api_key=client.api_client.default_headers["Authorization"],
+        token = AccessToken.from_client_cookies(
+            cookies=client.api_client.cookies,
+            headers=client.api_client.default_headers,
         )
-        encoded_token_data = base64.b64encode(json.dumps(token_data).encode()).decode()
 
-        sessionid_expiry = client.api_client.cookies["sessionid"]["expires"]
+        encoded_token = token.serialize()
+        print(f"Your machine token is:\n{encoded_token}\n")
+        print(f"It expires at {token.expires_at}.")
 
-        print(f"Your machine token is:\n{encoded_token_data}\n")
-        print(f"It expires at {sessionid_expiry}.")
+        return encoded_token
 
 
 def test_create_token():
-    from pydantic_settings import BaseSettings
-
-    class Settings(
-        BaseSettings, env_prefix="CVAT_", env_file=".env.cvat.secrets", extra="ignore"
-    ):
+    class Settings(BaseSettings):
         username: str
         password: str
 
-    settings = Settings()
+        class Config:
+            env_prefix = "CVAT_"
+            env_file = ".env.cvat.secrets"
 
+    settings = Settings()
     create_token(username=settings.username, password=settings.password)
