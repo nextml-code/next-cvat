d8b4f246a8bd65c3f1f45a735c2265e600841c3c
Richard Löwenström <samedii@gmail.com>
2024-11-05 17:32:21 +0100

    refactor: typer app in folder, reuse client for download and token creation, and more

--- next_cvat/__init__.py
+++ next_cvat/__init__.py
@@ -1,4 +1,3 @@
 from .annotations import Annotations
 from .client import Client
-from .download import download
 from .types import Attribute, Box, Label, LabelAttribute, Mask, Polygon, Project, Task
