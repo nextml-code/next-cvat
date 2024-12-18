fb701319a28f0adb385123f9410a3b738d8de954
FelixAbrahamsson <felixabrahamsson1@gmail.com>
2024-12-18 18:53:45 +0100

    improve: support value none in attribute

--- next_cvat/types/attribute.py
+++ next_cvat/types/attribute.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
+from typing import Optional
+
 from pydantic import BaseModel
 
 
 class Attribute(BaseModel):
     name: str
-    value: str
+    value: Optional[str]
