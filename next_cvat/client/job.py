d2b73ec074ab5557e669df72eb23afe5fbd7d172
Richard Löwenström <samedii@gmail.com>
2024-12-28 18:03:07 +0100

    feature: job state and stage

--- next_cvat/client/job.py
+++ next_cvat/client/job.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
 from contextlib import contextmanager
-from typing import TYPE_CHECKING, Generator
+from typing import TYPE_CHECKING, Generator, Literal
 
-from cvat_sdk.api_client import models
+from cvat_sdk.core.proxies.jobs import Job as CVATJob
 from pydantic import BaseModel
 
 from .job_annotations import JobAnnotations
@@ -12,12 +12,16 @@ if TYPE_CHECKING:
     from .task import Task
 
 
+JobState = Literal["new", "in progress", "rejected", "completed"]
+JobStage = Literal["annotation", "validation", "acceptance"]
+
+
 class Job(BaseModel):
     task: Task
     id: int
 
     @contextmanager
-    def cvat(self) -> Generator[models.TaskRead, None, None]:
+    def cvat(self) -> Generator[CVATJob, None, None]:
         with self.task.project.client.cvat_client() as cvat_client:
             yield cvat_client.jobs.retrieve(self.id)
 
@@ -37,5 +41,12 @@ class Job(BaseModel):
                 annotations_request
             )
 
-    def job(self, job_id: int) -> Job:
-        return Job(task=self, id=job_id)
+    def state(self) -> JobState:
+        """Get the current state of the job (e.g., 'new', 'in progress', 'completed', 'rejected')."""
+        with self.cvat() as job:
+            return job.state
+
+    def stage(self) -> JobStage:
+        """Get the current stage of the job (e.g., 'annotation', 'validation', 'acceptance')."""
+        with self.cvat() as job:
+            return job.stage
